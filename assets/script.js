6effe359bd355061524ffa337ce55d89d545b145
adam willetts <adam-willetts@hotmail.co.uk>
2023-02-04 15:52:07 +0000

    added weather api. but havent got it work using long and lat variables yet

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-
 let searchBtn = document.querySelector("#search-button");
 apiKey = "b3c45a3d775f40413927ce1c2d7bf921";
 let longitude;
+let latitude;
 
 searchBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    let userInput = document.querySelector("#search-input").value;
-    // console.log(userInput);
+  e.preventDefault();
+  let userInput = document.querySelector("#search-input").value;
+  // console.log(userInput);
   fetch(
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
       userInput +
@@ -14,10 +14,26 @@ searchBtn.addEventListener("click", function (e) {
       apiKey
   )
     .then((response) => response.json())
-    .then((city) =>  {
-      let longitude = city[0].lon
-      let latitude = city[0].lat
-          console.log(longitude);
-          console.log(latitude)
+    .then((city) => {
+      longitude = city[0].lon;
+      latitude = city[0].lat;
+      console.log(longitude);
+      console.log(latitude);
+    });
+    getWeather();
+});
 
-})});
+
+function getWeather (){
+fetch(
+  "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&appid=" +
+    apiKey +
+    "&units=metric"
+)
+  .then((response) => response.json())
+  .then((weather) => console.log(weather));
+};
